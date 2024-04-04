d785e62ce314af9e5c41bda8ce5e8a02adbd7f7e
Bri Halterman <halterma@gmail.com>
2024-04-04 14:24:00 -0400

    Review basic JavaScript methods

--- app.js
+++ app.js
@@ -1,2 +1,68 @@
 const numbers = ['one', 'two', 'three', 'four'];
 const colors = ['red', 'green', 'blue'];
+
+console.log(numbers);
+console.log(colors);
+
+// console.log(numbers);
+// // returns [ 'one', 'two', 'three', 'four' ]
+// console.log(numbers.length);
+// // returns 4
+
+// numbers.push("five");
+// console.log(numbers)
+// // returns ['one', 'two', 'three', 'four', 'five']
+// console.log(numbers.length)
+// // returns 5
+
+// numbers.pop();
+// console.log(numbers);
+// // returns ['one', 'two', 'three']
+
+// const lastNum = numbers.pop();
+// console.log(lastNum);
+// // returns four
+
+// console.log(numbers.indexOf('three'));
+// // returns 2
+
+// colors.shift();
+// console.log(colors);
+// // returns ['green', 'blue']
+
+// const shiftedElement = colors.shift();
+// console.log(shiftedElement);
+// // returns red
+
+// colors.unshift("purple");
+// console.log(colors);
+// // returns ['purple', 'red', 'green', 'blue']
+
+// colors.push('purple');
+// console.log(colors);
+// // returns ['red', 'green', 'blue', 'purple']
+
+// console.log(colors);
+// // returns ['red', 'green', 'blue']
+
+// console.log(colors.toString());
+// // returns red,green,blue
+
+// console.log(colors.join(' '))
+// // return red green blue
+
+// console.log(colors.join(', '));
+// // returns red, green, blue
+
+// console.log(colors.join(' * '))
+// // returns red * green * blue
+
+// const data = numbers.concat(colors);
+// console.log(data)
+// // returns ['one', 'two, 'three', 'four', 'red', 'green', 'blue']
+// console.log(data.length);
+// // returns 7
+
+// const data = colors.concat(numbers);
+// console.log(data);
+// // returns ['red', 'green', 'blue', 'one', 'two', 'three', 'four']
